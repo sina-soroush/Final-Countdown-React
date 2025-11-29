60adbcff357a5178f480bb79f09e31c8c71917bd
Sina <sina.soroush@live.com>
2025-11-29 23:46:33 +0100

    chore: add .nojekyll to prevent Jekyll processing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Final-Countdown-React/',
-  plugins: [react()],
+  base: '/Final-Countdown-React/', 
+  plugins: [react()]
 })
